4031f4d76ad86109bc9533ee8cb2bb7645886810
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-11-19 20:51:03 +0200

    added hero bg

--- src/components/header/Hero.js
+++ src/components/header/Hero.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import img from "./image1.png";
+import img from "./image1.gif";
+import GetOfferBtn from '../GetOfferBtn';
+import "./style.css"
 
 const Hero = () => {
-    const clipPath = `polygon(0% 0%, 100% 0%, 100% 90%, 50% 100%, 0% 90%)`;
+    const clipPath = `polygon(1500px 0px, 0px 0px, calc(50% - 750px) 700px, 1147.04px 824.453px, 1147.04px 824.453px, 1151.73359px 824.835999px, 1156.42952px 824.994712px, 1161.11753px 824.930513px, 1165.78736px 824.644776px, 1170.42875px 824.138875px, 1175.03144px 823.414184px, 1179.58517px 822.472077px, 1184.07968px 821.313928px, 1188.50471px 819.941111px, 1192.85px 818.355px, 1500px 700px, 1500px 0px)`; 
 
     return (
         <div className="relative" style={{ width: '1500px', height: '925px' }}>
@@ -11,20 +13,24 @@ const Hero = () => {
                 backgroundImage: `url(${img})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
-                clipPath: clipPath, // Apply the clipping path here
+                clipPath: clipPath,
                 zIndex: '1'
             }}></div>
 
-            {/* Content container */}
             <div className="relative z-10">
-                {/* Content goes here */}
+                <h2>Sell Your Car Today!</h2>
+                <h1>Get More than Just an “Instant Cash Offer”</h1>
+                <h2>Drive In, Cash Out!</h2>
+                <GetOfferBtn />
+
+
             </div>
 
-            <svg className="absolute bottom-0 w-full z-10" height="125" viewBox="0 0 1500 125" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1147.04 124.453L0 0L1500 0L1192.85 118.355C1178.51 123.996 1162.67 126.105 1147.04 124.453Z" fill="transparent"/>
-            </svg>
         </div>
     );
 }
+
+
+
 
 export default Hero;

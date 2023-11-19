add092a7d163aa9d319a77c01c33583960bd566e
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-11-19 10:56:41 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'deep-crimson': '#D11142', 
+        'royal-blue': '#385CAD',  
+        'midnight-blue': '#09183B',
+        'sky-blue': '#4280EF',    
+      },
+      fontFamily: {
+        sans: ['Avenir Next Pro', 'Helvetica', 'Arial', 'sans-serif'],
+        'serif': ['Georgia', 'Times New Roman', 'serif'],
+      },
+    },
+  },
+  variants: {
+    // Configure the variants here
+    // For example, you can enable 'disabled' or 'visited' variants for certain utilities
+    extend: {},
+  },
+  plugins: [
+    // Add any Tailwind plugins you wish to use here
+    // For example: require('@tailwindcss/forms'),
+  ],
+}
